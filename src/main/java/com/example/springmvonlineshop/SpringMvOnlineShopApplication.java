856ae00c66fba1dda3671d6ae0ef00ef2527f4f5
ThetMyatNoe96 <vegamyatnoe@gmail.com>
2020-01-25 22:15:40 +0630

    MyFirstCommit

---
+++ src/main/java/com/example/springmvonlineshop/SpringMvOnlineShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvonlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvOnlineShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvOnlineShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springmvonlineshop/SpringMvOnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvonlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvOnlineShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
